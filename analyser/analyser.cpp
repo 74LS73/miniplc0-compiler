9018032be39ec4971c3e7dbb448a40ec1cb38157
74LS73 <664185376@qq.com>
2020-12-21 15:15:14 +0800

    nextToken和unreadToken忽略comment

--- analyser/analyser.cpp
+++ analyser/analyser.cpp
@@ -13,17 +13,24 @@ Analyser::Analyse() {
     return std::make_pair(_instructions, std::optional<CompilationError>());
 }
 
-
+// 返回下一个Token
 std::optional<Token> Analyser::nextToken() {
   if (_offset == _tokens.size()) return {};
   // 考虑到 _tokens[0..._offset-1] 已经被分析过了
   // 所以我们选择 _tokens[0..._offset-1] 的 EndPos 作为当前位置
   _current_pos = _tokens[_offset].GetEndPos();
+  // 自动忽略comment
+  while (_tokens[_offset].GetType() == TokenType::COMMENT)
+    ++_offset;
   return _tokens[_offset++];
 }
 
+// 回退上一个Token
 void Analyser::unreadToken() {
   if (_offset == 0) DieAndPrint("analyser unreads token from the begining.");
+  // 自动忽略comment
+  while (_tokens[_offset - 1].GetType() == TokenType::COMMENT)
+    --_offset;
   _current_pos = _tokens[_offset - 1].GetEndPos();
   _offset--;
 }
